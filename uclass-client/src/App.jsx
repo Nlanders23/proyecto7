1c0fbdd7763bd69bd198147e33b57c7eadef0784
NicoLanders <nlandero23@gmail.com>
2025-02-27 16:52:01 -0300

    commit 4: creación del header

--- uclass-client/src/App.jsx
+++ uclass-client/src/App.jsx
@@ -6,6 +6,7 @@ import ClothsState from './Context/Cloths/ClothsState'
 import Profile from './components/Profile/Profile'
 import Register from './components/Register/Register'
 import Login from './components/Login/Login'
+import ClothList from './components/Cloths/ClothList'
 import UserState from './Context/Users/UserState'
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
             <Route path='/iniciar-sesion' element={<Login />} />
             { /*RUTAS PÚBLICAS */}
             <Route path='/' element={<Home />} />
+            <Route path='/catalogo-de-productos' element={<ClothList />} />
           </Routes>
         </Router>
       </ClothsState>
--- uclass-client/src/components/home/Home.jsx
+++ uclass-client/src/components/home/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import ClothList from '../Cloths/ClothList'
 
 const Home = () => {
   return (
     <>
         <p>Este es el home</p>
-        <ClothList />
     </>
   )
 }
